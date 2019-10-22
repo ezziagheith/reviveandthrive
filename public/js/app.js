f8ae1dc37da3bb7be2e4a1e8aace5760a5828833
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-10-22 15:05:43 -0700

    event routes is working with db

--- public/js/app.js
+++ public/js/app.js
@@ -21,12 +21,13 @@ onSuccess = (response) => {
     console.log('success!');
   
     response.data.forEach((events) => {
-      const template = `
+      console.log(events);
+        const template = `
       <table>
         <td>
             <em>${events.time}</em>
             <h5>${events.className}</h5>
-            <p>${events.teacherNmae}</p> 
+            <p>${events.teacherName}</p> 
         <section class="details">
             <small>Click for Details</small>
             <p class="hide">${events.description}</p>     
