617691bd1cee729589d065cbfce7672aab55bf3c
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-10-24 11:28:56 -0700

    admin works

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,8 @@ router.post('/signup', ctrl.newUser.createUser);
 router.post('/login', ctrl.auth.createSession);
 router.get('/account/:id', ctrl.account.showAccount);
 router.put('/users/:id', ctrl.newUser.updateUser);
-router.delete('/users/:id', ctrl.newUser.destroy);
-router.delete('/events/:id', ctrl.account.destroy);
+router.delete('/user/:id', ctrl.newUser.destroy);
+router.delete('/events/:id', ctrl.event.destroy);
 
 
 
@@ -21,5 +21,6 @@ router.delete('/events/:id', ctrl.account.destroy);
 router.get('/events', ctrl.event.showEvent);
 router.post('/events', ctrl.event.createEvent);
 router.put('/events/:id', ctrl.event.updateEvent);
+
 
 module.exports = router;
